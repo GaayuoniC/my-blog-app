c06f79d97934389c7e70351f7a0ad8ae5f6d1cd7
christian <gayuoni@proton.me>
2024-06-18 09:53:15 +0200

    Data modified with sample info

--- src/lib/data.js
+++ src/lib/data.js
@@ -12,9 +12,9 @@ const users=[
 
 const posts=[
     {id:1, title:"Post 1", body:".....", userId:1},
-    {id:2, title:"Post 1", body:".....", userId:2},
-    {id:3, title:"Post 1", body:".....", userId:3},
-    {id:4, title:"Post 1", body:".....", userId:4},
+    {id:2, title:"Post 2", body:".....", userId:2},
+    {id:3, title:"Post 3", body:".....", userId:3},
+    {id:4, title:"Post 4", body:".....", userId:4},
 
 
 ]
